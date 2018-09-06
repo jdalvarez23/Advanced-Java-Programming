da4cf1084fb8fa16679da0b7855bc760846cae32
jalvarez343 <jalvarez343@WRL-00L123-L013.ccc.edu>
2018-09-06 13:44:26 -0500

    Update 09/06/2018 - Added SplitFunction class

---
+++ src/CIS244/SplitFunction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CIS244;
+
+/**
+ *
+ * @author jalvarez343
+ */
+public class SplitFunction {
+    
+    public static void main(String[] args) {
+        String str = "alpha beta game.gone";
+        String [] tokens = str.split(" ");
+    
+        // for enhanced to show tokens
+        for (String token: tokens) {
+            System.out.println(token);
+        }
+        
+        // standard for loop
+        // display the array
+        for (int i = 0; i < tokens.length; i++) {
+            System.out.println(tokens[i]);
+        }
+    }
+    
+}
